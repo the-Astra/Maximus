42788dc99e0bf6166754c83702a9a08e9ccd113a
DJ Stalder <djs8743@rit.edu>
2024-12-28 00:45:27 -0500

    Added Abyss; Updated sprites

--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ SMODS.Joker { -- Fortune Cookie
     perishable_compat = false,
     eternal_compat = false,
     config = { extra = {
-            chance = 1
+            chance = 10
         }
     },
     loc_vars = function(self,info_queue,center)
@@ -50,6 +50,7 @@ SMODS.Joker { -- Fortune Cookie
                         return true; end
                     }))
                     return {
+                        card = card,
                         message = 'FORTUNATE!',
                         colour = G.C.SECONDARY_SET.Tarot
                     }
@@ -61,6 +62,7 @@ SMODS.Joker { -- Fortune Cookie
                         return true; end
                     }))
                     return {
+                        card = card,
                         message = 'NOPE!',
                         colour = G.C.SET.Tarot
                     }
@@ -73,6 +75,7 @@ SMODS.Joker { -- Fortune Cookie
                     return true; end
                 }))
                 return {
+                    card = card,
                     message = 'WASTED',
                     colour = G.C.SET.Tarot
                 }
@@ -80,6 +83,7 @@ SMODS.Joker { -- Fortune Cookie
 
             card:juice_up(0.3, 0.4)
             return {
+                card = card,
                 message = '-1',
                 colour = G.C.RED
             }
@@ -105,6 +109,7 @@ SMODS.Joker { -- Fortune Cookie
                     end
                 }))
                 return {
+                    card = card,
                     message = 'Crumbled'
                 } 
             end
@@ -139,6 +144,7 @@ SMODS.Joker { -- Poindexter
         if context.joker_main then
             if card.ability.extra.Xmult > 0 then
                 return {
+                    card = card,
                     Xmult_mod = card.ability.extra.Xmult,
                     message = 'x' .. card.ability.extra.Xmult,
                     colour = G.C.MULT
@@ -176,6 +182,7 @@ SMODS.Joker { -- Poindexter
         if context.after and not context.blueprint then
             if card.ability.extra.shattered then
                 return {
+                    card = card,
                     message = 'Errrrmmm...',
                     colour = G.C.RED
                 }
@@ -194,12 +201,92 @@ SMODS.Joker { -- Poindexter
                         card.ability.extra.Xmult = card.ability.extra.Xmult + (glass * 0.25)
                         card:juice_up(0.3, 0.4)
                         play_sound('tarot1')
-                        return true; end
-                    }))
+                        return true; 
+                    end
+                }))
                 return {
+                    card = card,
                     message = 'Eureka!',
                     colour = G.C.MULT
                 }
+            end
+        end
+    end
+}
+
+SMODS.Joker { -- Abyss
+    key='abyss',
+    loc_txt = {
+        name = "Abyss",
+        text = {
+            'When blind is selected, {C:attention}50/50{}',
+            '{C:attention}chance{} of either creating',
+            'a random {C:dark_edition}Negative Joker{} or destroying',
+            'a currently held non-Negative Joker'
+        }
+    },
+    atlas = 'Jokers',
+    rarity = 3,
+    pos = {x = 2, y = 0},
+    config = {},
+    loc_vars = function(self,info_queue,center)
+        info_queue[#info_queue+1] = G.P_CENTERS.e_negative
+        return {vars = {}}
+    end,
+    calculate = function(self,card,context)
+        if context.setting_blind then
+            local flip = pseudorandom('aby', 1, 2)
+            sendDebugMessage(flip, "MaximusDebug")
+
+            -- Create a random negative joker
+            if flip == 1 then
+                local new_card = create_card('Joker', G.jokers, nil, nil, nil, nil, nil, 'aby')
+                new_card:add_to_deck()
+                G.jokers:emplace(new_card)
+                new_card:set_edition({negative = true}, true)
+                card:juice_up(0.3, 0.4)
+                play_sound('foil1')
+                return {
+                    card = card,
+                    message = 'Void-touched!',
+                    colour = G.C.PURPLE
+                }
+            
+            -- Destroy a random non-negative joker
+            elseif flip == 2 then
+                -- Code derived Madness
+                local deletable_jokers = {}
+                for i = 1, #G.jokers.cards do
+                    if G.jokers.cards[i] ~= card and not G.jokers.cards[i].ability.eternal and not(G.jokers.cards[i].edition and G.jokers.cards[i].edition.negative) and not G.jokers.cards[i].getting_sliced then 
+                        deletable_jokers[#deletable_jokers + 1] = G.jokers.cards[i] 
+                    end
+                end
+                if next(deletable_jokers) == nil then
+                    sendDebugMessage('No Deletable Jokers for Abyss', "MaximusDebug")
+                    return {
+                        card = card,
+                        message = 'No target...',
+                        colour = G.C.PURPLE
+                    }
+                else 
+                    local chosen_joker = #deletable_jokers > 0 and pseudorandom_element(deletable_jokers, pseudoseed('abyss')) or nil
+
+                    if chosen_joker and not (context.blueprint_card or card).getting_sliced then
+                        chosen_joker.getting_sliced = true
+                        G.E_MANAGER:add_event(Event({
+                            func = function()
+                                (context.blueprint_card or card):juice_up(0.8, 0.8)
+                                chosen_joker:start_dissolve({G.C.PURPLE}, nil, 1.6)
+                                return true; 
+                            end 
+                        }))
+                        return {
+                            card = card,
+                            message = 'Consumed',
+                            colour = G.C.PURPLE
+                        }
+                    end
+                end
             end
         end
     end
