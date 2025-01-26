8187d9439f230aa33bf40cdb16a560f2935d21f8
DJ Stalder <djs8743@rit.edu>
2025-01-26 14:58:11 -0500

    Added Gravity

--- main.lua
+++ main.lua
@@ -3422,3 +3422,166 @@ SMODS.Joker { -- Glass Cannon
         end
     end
 }
+
+SMODS.Joker { -- Gravity
+    key = 'gravity',
+    loc_txt = {
+        name = 'Gravity',
+        text = { 'All Joker {X:mult,C:white}XMult{} is {C:attention}retriggered', '{C:attention}Shatters{} if blind isn\'t', 'beaten in 2 hands' }
+    },
+    atlas = 'Jokers',
+    pos = {
+        x = 8,
+        y = 6
+    },
+    rarity = 3,
+    config = {
+        extra = {
+            hands = 5
+        }
+    },
+    blueprint_compat = false,
+    eternal_compat = false,
+    cost = 6,
+    calculate = function(self, card, context)
+        if context.end_of_round and not context.blueprint and not context.repetition and not context.individual then
+            update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3 },
+                { handname = localize('k_all_hands'), chips = '...', mult = '...', level = '' })
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.2,
+                func = function()
+                    play_sound('tarot2')
+                    card:juice_up(0.8, 0.5)
+                    G.TAROT_INTERRUPT_PULSE = true
+                    return true
+                end
+            }))
+            update_hand_text({ delay = 0 }, { mult = '-', StatusText = true })
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.9,
+                func = function()
+                    play_sound('tarot2')
+                    card:juice_up(0.8, 0.5)
+                    return true
+                end
+            }))
+            update_hand_text({ delay = 0 }, { chips = '-', StatusText = true })
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.9,
+                func = function()
+                    play_sound('tarot2')
+                    card:juice_up(0.8, 0.5)
+                    G.TAROT_INTERRUPT_PULSE = nil
+                    return true
+                end
+            }))
+            update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.9, delay = 0 }, { level = '-1' })
+            delay(1.3)
+            for k, v in pairs(G.GAME.hands) do
+                level_up_hand(self, k, true, -1)
+            end
+            update_hand_text({ sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
+                { mult = 0, chips = 0, handname = '', level = '' })
+            card.ability.extra.hands = card.ability.extra.hands - 1
+
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    delay = 0.9,
+                    func = function()
+                        if card.ability.extra.hands == 0 then
+                            card:start_dissolve({ G.C.RED }, nil, 1.6)
+                            card_eval_status_text(card, 'extra', nil, nil, nil,
+                                { message = 'Splat!', colour = G.C.RED })
+                        end
+                        return true
+                    end
+                }))
+        end
+    end,
+    add_to_deck = function(self, card, from_debuff)
+        update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3 },
+            { handname = localize('k_all_hands'), chips = '...', mult = '...', level = '' })
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.2,
+            func = function()
+                play_sound('tarot1')
+                card:juice_up(0.8, 0.5)
+                G.TAROT_INTERRUPT_PULSE = true
+                return true
+            end
+        }))
+        update_hand_text({ delay = 0 }, { mult = '+', StatusText = true })
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.9,
+            func = function()
+                play_sound('tarot1')
+                card:juice_up(0.8, 0.5)
+                return true
+            end
+        }))
+        update_hand_text({ delay = 0 }, { chips = '+', StatusText = true })
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.9,
+            func = function()
+                play_sound('tarot1')
+                card:juice_up(0.8, 0.5)
+                G.TAROT_INTERRUPT_PULSE = nil
+                return true
+            end
+        }))
+        update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.9, delay = 0 }, { level = '+5' })
+        delay(1.3)
+        for k, v in pairs(G.GAME.hands) do
+            level_up_hand(self, k, true, 5)
+        end
+        update_hand_text({ sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
+            { mult = 0, chips = 0, handname = '', level = '' })
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+        if card.ability.extra.hands > 0 then
+            update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3 },
+                { handname = localize('k_all_hands'), chips = '...', mult = '...', level = '' })
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.2,
+                func = function()
+                    play_sound('tarot2')
+                    G.TAROT_INTERRUPT_PULSE = true
+                    return true
+                end
+            }))
+            update_hand_text({ delay = 0 }, { mult = '-', StatusText = true })
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.9,
+                func = function()
+                    play_sound('tarot2')
+                    return true
+                end
+            }))
+            update_hand_text({ delay = 0 }, { chips = '-', StatusText = true })
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.9,
+                func = function()
+                    play_sound('tarot2')
+                    G.TAROT_INTERRUPT_PULSE = nil
+                    return true
+                end
+            }))
+            update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.9, delay = 0 }, { level = '-'..card.ability.extra.hands })
+            delay(1.3)
+            for k, v in pairs(G.GAME.hands) do
+                level_up_hand(self, k, true, -(card.ability.extra.hands))
+            end
+            update_hand_text({ sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
+                { mult = 0, chips = 0, handname = '', level = '' })
+        end
+    end
+}
