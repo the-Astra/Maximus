53c09d8d08a9177e99a751de9ae05b3b6e95c6a4
DJ Stalder <djs8743@rit.edu>
2025-01-26 00:23:09 -0500

    Added Endless Breadsticks

--- main.lua
+++ main.lua
@@ -111,6 +111,9 @@ food_jokers = { {
 }, {
     key = 'j_mxms_leftovers',
     name = 'Leftovers'
+}, {
+    key = 'j_mxms_breadsticks',
+    name = 'Endless Breadsticks'
 } }
 
 
@@ -3232,7 +3235,7 @@ SMODS.Joker { -- Group Chat
         if context.joker_main and card.ability.extra.chips > 0 then
             return {
                 chip_mod = card.ability.extra.chips,
-                message = '+'..card.ability.extra.chips,
+                message = '+' .. card.ability.extra.chips,
                 colour = G.C.CHIPS,
                 card = card
             }
@@ -3265,7 +3268,6 @@ SMODS.Joker { -- Minimalist
         }
     end,
     calculate = function(self, card, context)
-
         card.ability.extra.chips = 90
         for k, v in pairs(G.playing_cards) do
             if next(SMODS.get_enhancements(v)) and card.ability.extra.chips > 0 then
@@ -3276,7 +3278,7 @@ SMODS.Joker { -- Minimalist
         if context.joker_main and card.ability.extra.chips > 0 then
             return {
                 chip_mod = card.ability.extra.chips,
-                message = '+'..card.ability.extra.chips,
+                message = '+' .. card.ability.extra.chips,
                 colour = G.C.CHIPS,
                 card = card
             }
@@ -3284,3 +3286,71 @@ SMODS.Joker { -- Minimalist
     end
 }
 
+SMODS.Joker { -- Endless Breadsticks
+    key = 'breadsticks',
+    loc_txt = {
+        name = 'Endless Breadsticks',
+        text = { 'Gains {C:chips}+20{} Chips every {C:attention}#1#{} cards', 'discarded this round. Discard requirement', 'increases by {C:attention}1{} each round', '{C:inactive}Currently: {C:chips}+#2#' }
+    },
+    atlas = 'Jokers',
+    pos = {
+        x = 6,
+        y = 6
+    },
+    rarity = 1,
+    config = {
+        extra = {
+            d_requirement = 2,
+            d_tally = 0,
+            chips = 0
+        }
+    },
+    blueprint_compat = false,
+    cost = 4,
+    loc_vars = function(self, info_queue, center)
+        return {
+            vars = { center.ability.extra.d_requirement, center.ability.extra.chips }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.discard and not context.blueprint then
+            card.ability.extra.d_tally = card.ability.extra.d_tally + 1
+            if card.ability.extra.d_tally < card.ability.extra.d_requirement then
+                return {
+                    delay = 0.2,
+                    message =  card.ability.extra.d_tally .. '/' .. card.ability.extra.d_requirement,
+                    colour = G.C.CHIPS,
+                    card = card
+                }
+            else
+                card.ability.extra.chips = card.ability.extra.chips + 20
+                card.ability.extra.d_tally = 0
+                return {
+                    delay = 0.2,
+                    message =  localize('k_upgrade_ex'),
+                    colour = G.C.CHIPS,
+                    card = card
+                }
+            end
+        end
+
+        if context.joker_main and card.ability.extra.chips > 0 then
+            return {
+                chip_mod = card.ability.extra.chips,
+                message = '+' .. card.ability.extra.chips,
+                colour = G.C.CHIPS,
+                card = card
+            }
+        end
+
+        if context.end_of_round and not context.blueprint and not context.repetition and not context.individual then
+            card.ability.extra.d_tally = 0
+            card.ability.extra.d_requirement = card.ability.extra.d_requirement + 1
+            return {
+                message = 'Requirement Up',
+                colour = G.C.CHIPS,
+                card = card
+            }
+        end
+    end
+}
