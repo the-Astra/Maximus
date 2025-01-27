8f6ad6d73f0796ab9b46a4426a1679026be59d24
DJ Stalder <djs8743@rit.edu>
2025-01-26 23:28:10 -0500

    Added Fog

--- main.lua
+++ main.lua
@@ -262,6 +262,28 @@ SMODS.Edition:take_ownership('foil', {
     end
 })
 
+-- Change 4oaK and 2P to work with Fog
+SMODS.PokerHand:take_ownership('Four of a Kind', {
+    evaluate = function(parts, hand)
+        if #parts._2 == 2 and next(SMODS.find_card('j_mxms_fog')) then
+            local pair_1 = parts._2[1]
+            local pair_2 = parts._2[2]
+            if math.abs(pair_1[1]:get_id() - pair_2[2]:get_id()) == 1 or (pair_1[1]:get_id() == 14 and pair_2[1]:get_id() == 2) then
+                return parts._all_pairs
+            end
+        end
+        return parts._4
+    end
+})
+
+SMODS.PokerHand:take_ownership('Two Pair', {
+    evaluate = function(parts, hand)
+        if next(parts._4) and next(SMODS.find_card('j_mxms_fog')) then return parts._4 end
+        if #parts._2 < 2 then return {} end
+        return parts._all_pairs
+    end
+})
+
 
 -- Update checks
 
@@ -3582,4 +3604,21 @@ SMODS.Joker { -- Gravity
                 { mult = 0, chips = 0, handname = '', level = '' })
         end
     end
+}
+
+SMODS.Joker { -- Fog
+    key = 'fog',
+    loc_txt = {
+        name = 'Fog',
+        text = { '{C:attention}Four of a Kinds{} contain {C:attention}Two Pairs{}', 'Two Pairs with a {C:attention}1-rank difference{} count', 'as Four of a Kinds {C:inactive}(ex. 6 6 5 5)' }
+    },
+    atlas = 'Jokers',
+    pos = {
+        x = 0,
+        y = 9
+    },
+    blueprint_compat = false,
+    cost = 5,
+    rarity = 2,
+    config = {}
 }
