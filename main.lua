d0b247b958fb6c1c08bb83ed50a14098d931fe8a
DJ Stalder <djs8743@rit.edu>
2025-01-27 00:15:30 -0500

    Added Stone Thrower

--- main.lua
+++ main.lua
@@ -3378,7 +3378,7 @@ SMODS.Joker { -- Endless Breadsticks
             card.ability.extra.d_tally = 0
             card.ability.extra.d_requirement = card.ability.extra.d_requirement + 1
             return {
-                message = 'Requirement Up',
+                message = 'More Please!',
                 colour = G.C.CHIPS,
                 card = card
             }
@@ -3621,4 +3621,48 @@ SMODS.Joker { -- Fog
     cost = 5,
     rarity = 2,
     config = {}
+}
+
+SMODS.Joker { -- Stone Thrower
+    key = 'stone_thrower',
+    loc_txt = {
+        name = 'Stone Thrower',
+        text = { 'Gains {C:chips}+30{} Chips for every', 'scored {C:attention}glass card{}', 'Glass cards are {C:attention}guaranteed to break{}', '{C:inactive}Currently: {C:chips}+#1#' }
+    },
+    atlas = 'Jokers',
+    pos = {
+        x = 1,
+        y = 9
+    },
+    rarity = 1,
+    config = {
+        extra = {
+            chips = 0
+        }
+    },
+    blueprint_compat = false,
+    enhancement_gate = 'm_glass',
+    cost = 3,
+    loc_vars = function(self, info_queue, center)
+        info_queue[#info_queue + 1] = G.P_CENTERS.m_glass
+        return {
+            vars = { center.ability.extra.chips }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main and card.ability.extra.chips > 0 then
+            return {
+                chip_mod = card.ability.extra.chips,
+                message = '+' .. card.ability.extra.chips,
+                colour = G.C.CHIPS,
+                card = card
+            }
+        end
+
+        if context.individual and context.cardarea == G.play and context.other_card.config.center == G.P_CENTERS.m_glass then
+            card.ability.extra.chips = card.ability.extra.chips + 30
+            card_eval_status_text(card, 'extra', nil, nil, nil,{ message = localize('k_upgrade_ex'), colour = G.C.CHIPS })
+        end 
+
+    end
 }
