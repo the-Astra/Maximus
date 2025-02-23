c20d39a6c46087530a1618e09fdacb391168e424
DJ Stalder <djs8743@rit.edu>
2025-02-23 02:37:29 -0500

    Buffed Down the Drain challenge

--- items/challenges/drain.lua
+++ items/challenges/drain.lua
@@ -3,7 +3,11 @@ SMODS.Challenge {
     loc_txt = {
         name = 'Down the Drain'
     },
-    rules = {},
+    rules = {
+        custom = {
+            { id = 'mxms_X_blind_size', value = 3 }
+        }
+    },
     jokers = {
         { id = 'j_smeared',    eternal = true },
         { id = 'j_mxms_faded', eternal = true }
