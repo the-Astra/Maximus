31c0057d1aa4c5351202b58f2aae52c6cae78d20
djs8743 <75325082+the-Astra@users.noreply.github.com>
2025-02-27 14:20:55 -0500

    Migrated hangman code from patch to hook

--- items/jokers/hangman.lua
+++ items/jokers/hangman.lua
@@ -13,3 +13,97 @@ SMODS.Joker {
     blueprint_compat = false,
     cost = 6,
 }
+
+local gs = get_straight
+
+function get_straight(hand)
+    if not next(SMODS.find_card("j_mxms_hangman")) then return gs(hand) end
+
+    local ret = {}
+	local four_fingers = next(SMODS.find_card('j_four_fingers'))
+	local can_skip = next(SMODS.find_card('j_shortcut'))
+    local has_face_filled = false
+	if #hand < (5 - (four_fingers and 1 or 0)) then return ret end
+	local t = {}
+	local RANKS = {}
+	for i = 1, #hand do
+		if hand[i]:get_id() > 0 then
+			local rank = hand[i].base.value
+			RANKS[rank] = RANKS[rank] or {}
+			RANKS[rank][#RANKS[rank] + 1] = hand[i]
+		end
+	end
+
+	local straight_length = 0
+	local straight = false
+	local skipped_rank = false
+	local vals = {}
+	for k, v in pairs(SMODS.Ranks) do
+		if v.straight_edge then
+			table.insert(vals, k)
+		end
+	end
+	local init_vals = {}
+	for _, v in ipairs(vals) do
+		init_vals[v] = true
+	end
+	if not next(vals) then table.insert(vals, 'Ace') end
+	local initial = true
+	local br = false
+	local end_iter = false
+	local i = 0
+	while 1 do
+		end_iter = false
+		if straight_length >= (5 - (four_fingers and 1 or 0)) then
+			straight = true
+		end
+		i = i + 1
+		if br or (i > #SMODS.Rank.obj_buffer + 1) then break end
+		if not next(vals) then break end
+		for _, val in ipairs(vals) do
+			if init_vals[val] and not initial then br = true end
+			if RANKS[val] then
+				straight_length = straight_length + 1
+                if straight_pairs[val] then straight_length = straight_length + 1 end
+				skipped_rank = false
+				for _, vv in ipairs(RANKS[val]) do
+					t[#t + 1] = vv
+				end
+				vals = SMODS.Ranks[val].next
+				initial = false
+				end_iter = true
+				break
+			end
+		end
+		if not end_iter then
+			local new_vals = {}
+			for _, val in ipairs(vals) do
+				for _, r in ipairs(SMODS.Ranks[val].next) do
+					table.insert(new_vals, r)
+				end
+			end
+			vals = new_vals
+			if can_skip and not skipped_rank then
+				skipped_rank = true
+            elseif not has_face_filled then
+                for _, val in ipairs(vals) do
+                  if SMODS.Ranks[val].face or SMODS.Ranks[val].shorthand == 'A' then
+                    straight_length = straight_length + 1
+                    has_face_filled = true
+                    for _, r in ipairs(SMODS.Ranks[val].next) do
+                      table.insert(new_vals, r)
+                    end
+                  end
+                end
+			else
+				straight_length = 0
+				skipped_rank = false
+				if not straight then t = {} end
+				if straight then break end
+			end
+		end
+	end
+	if not straight then return ret end
+	table.insert(ret, t)
+	return ret
+end
