bf413a227229098043968d9ade951aa43777f829
DJ Stalder <djs8743@rit.edu>
2025-03-28 23:06:17 -0400

    Fixed Secret Society not handling dupes properly

--- items/jokers/secret_society.lua
+++ items/jokers/secret_society.lua
@@ -13,39 +13,43 @@ SMODS.Joker {
     cost = 5,
     rarity = 2,
     add_to_deck = function(self, card, from_debuff)
-        SMODS.Ranks['Ace'].nominal = 4
-        SMODS.Ranks['King'].nominal = 6
-        SMODS.Ranks['Queen'].nominal = 6
-        SMODS.Ranks['Jack'].nominal = 6
-        SMODS.Ranks['10'].nominal = 6
-        SMODS.Ranks['9'].nominal = 8
-        SMODS.Ranks['8'].nominal = 10
-        SMODS.Ranks['7'].nominal = 12
-        SMODS.Ranks['6'].nominal = 14
-        SMODS.Ranks['5'].nominal = 16
-        SMODS.Ranks['4'].nominal = 18
-        SMODS.Ranks['3'].nominal = 20
-        SMODS.Ranks['2'].nominal = 22
-        for k, v in ipairs(G.playing_cards) do
-            v.base.nominal = SMODS.Ranks[v.base.value].nominal
+        if not next(SMODS.find_card('j_mxms_secret_society')) then
+            SMODS.Ranks['Ace'].nominal = 4
+            SMODS.Ranks['King'].nominal = 6
+            SMODS.Ranks['Queen'].nominal = 6
+            SMODS.Ranks['Jack'].nominal = 6
+            SMODS.Ranks['10'].nominal = 6
+            SMODS.Ranks['9'].nominal = 8
+            SMODS.Ranks['8'].nominal = 10
+            SMODS.Ranks['7'].nominal = 12
+            SMODS.Ranks['6'].nominal = 14
+            SMODS.Ranks['5'].nominal = 16
+            SMODS.Ranks['4'].nominal = 18
+            SMODS.Ranks['3'].nominal = 20
+            SMODS.Ranks['2'].nominal = 22
+            for k, v in ipairs(G.playing_cards) do
+                v.base.nominal = SMODS.Ranks[v.base.value].nominal
+            end
         end
     end,
     remove_from_deck = function(self, card, from_debuff)
-        SMODS.Ranks['Ace'].nominal = 11
-        SMODS.Ranks['King'].nominal = 10
-        SMODS.Ranks['Queen'].nominal = 10
-        SMODS.Ranks['Jack'].nominal = 10
-        SMODS.Ranks['10'].nominal = 10
-        SMODS.Ranks['9'].nominal = 9
-        SMODS.Ranks['8'].nominal = 8
-        SMODS.Ranks['7'].nominal = 7
-        SMODS.Ranks['6'].nominal = 6
-        SMODS.Ranks['5'].nominal = 5
-        SMODS.Ranks['4'].nominal = 4
-        SMODS.Ranks['3'].nominal = 3
-        SMODS.Ranks['2'].nominal = 2
-        for k, v in ipairs(G.playing_cards) do
-            v.base.nominal = SMODS.Ranks[v.base.value].nominal
+        if not next(SMODS.find_card('j_mxms_secret_society')) then
+            SMODS.Ranks['Ace'].nominal = 11
+            SMODS.Ranks['King'].nominal = 10
+            SMODS.Ranks['Queen'].nominal = 10
+            SMODS.Ranks['Jack'].nominal = 10
+            SMODS.Ranks['10'].nominal = 10
+            SMODS.Ranks['9'].nominal = 9
+            SMODS.Ranks['8'].nominal = 8
+            SMODS.Ranks['7'].nominal = 7
+            SMODS.Ranks['6'].nominal = 6
+            SMODS.Ranks['5'].nominal = 5
+            SMODS.Ranks['4'].nominal = 4
+            SMODS.Ranks['3'].nominal = 3
+            SMODS.Ranks['2'].nominal = 2
+            for k, v in ipairs(G.playing_cards) do
+                v.base.nominal = SMODS.Ranks[v.base.value].nominal
+            end
         end
     end,
 }
