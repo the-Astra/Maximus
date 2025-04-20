f26607f81dded641232b1b6ccb39b6f7154c0811
DJ Stalder <djs8743@rit.edu>
2025-04-19 23:38:19 -0400

    misinterpreted autographed deck whoops

--- items/backs/autographed.lua
+++ items/backs/autographed.lua
@@ -3,9 +3,9 @@ SMODS.Back {
     loc_txt = {
         name = 'Autographed Deck',
         text = {
-            'Start run with',
-            '{C:attention}three{} Aces, Kings,',
-            'Queens, and Jacks {C:attention}per suit{}'
+            'Starting deck consists of',
+            '{C:attention}three{} Aces, Kings, Queens,',
+            'and Jacks {C:attention}per suit{}'
         }
     },
     atlas = 'Backs',
@@ -17,23 +17,28 @@ SMODS.Back {
         local extra_cards = {}
         for i, v in pairs(SMODS.Suits) do
             if type(v) == 'table' and type(v.in_pool) == 'function' and v.in_pool then
-                if v:in_pool({initial_deck = true}) then
+                if v:in_pool({ initial_deck = true }) then
                     for j = 1, 2 do
-                        extra_cards[#extra_cards+1] = {s = v.card_key, r = 'J'}
-                        extra_cards[#extra_cards+1] = {s = v.card_key, r = 'Q'}
-                        extra_cards[#extra_cards+1] = {s = v.card_key, r = 'K'}
-                        extra_cards[#extra_cards+1] = {s = v.card_key, r = 'A'}
+                        extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'J' }
+                        extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'Q' }
+                        extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'K' }
+                        extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'A' }
                     end
+                    --Extra Ace since they are not face cards
+                    extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'A' }
                 end
             else
                 for j = 1, 2 do
-                    extra_cards[#extra_cards+1] = {s = v.card_key, r = 'J'}
-                    extra_cards[#extra_cards+1] = {s = v.card_key, r = 'Q'}
-                    extra_cards[#extra_cards+1] = {s = v.card_key, r = 'K'}
-                    extra_cards[#extra_cards+1] = {s = v.card_key, r = 'A'}
+                    extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'J' }
+                    extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'Q' }
+                    extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'K' }
+                    extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'A' }
                 end
+                --Extra Ace since they are not face cards
+                extra_cards[#extra_cards + 1] = { s = v.card_key, r = 'A' }
             end
         end
         G.GAME.starting_params.extra_cards = extra_cards
+        G.GAME.starting_params.all_faces = true
     end
 }
