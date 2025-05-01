bc1ec3f478f9a14bc2173f16c97bb372ea9a7913
DJ Stalder <djs8743@rit.edu>
2025-04-30 21:10:37 -0400

    Contributors in credits

--- localization/default.lua
+++ localization/default.lua
@@ -1653,12 +1653,14 @@ return {
             mxms_credits = {
                 name = "",
                 text = {
-                    '{C:dark_edition,E:1,s:2}M A X I M U S',
+                    '{C:dark_edition,E:1,s:4}M A X I M U S',
                     ' ',
                     '{X:purple,C:white}Lead{} {X:purple,C:white}Programmer:{} {C:purple}theAstra',
                     '{X:attention,C:white}Lead{} {X:attention,C:white}Artist:{} {C:attention}Maxiss02',
                     ' ',
                     '{X:green,C:white}Supporting{} {X:green,C:white}Artists:{} {C:green}pinkzigzagoon, anerdymous, PsyAlola',
+                    ' ',
+                    '{X:planet,C:white}Contributors:{} {C:planet}sup3p, DigitalDetective47',
                     ' ',
                     '{X:gold,C:white}Special{} {X:gold,C:white}Thanks:{} All the awesome people in the Balatro Discord!',
                     '{C:white}You all helped make this project possible. Thank you guys for everything!'
