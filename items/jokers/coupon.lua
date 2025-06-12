7c434b5b111acb34186607d99e8204795ff9e0c6
DJ Stalder <djs8743@rit.edu>
2025-06-11 21:46:49 -0400

    Better Coupon implementation

--- items/jokers/coupon.lua
+++ items/jokers/coupon.lua
@@ -30,5 +30,18 @@ SMODS.Joker {
         W = W * (63 / 71)
         card.children.center.scale.x = card.children.center.scale.x * (63 / 71)
         card.T.w = W
-    end
+    end,
+    calculate = function(self, card, context)
+        local stg = card.ability.extra
+
+        if context.mxms_joker_cost_check and context.card.cost ~= 0 then
+            if pseudorandom('cou' .. G.GAME.round_resets.ante, stg.prob * G.GAME.probabilities.normal, stg.odds) == stg.odds then
+                context.card.cost = 0
+                card:juice_up(0.3, 0.4)
+            else
+                SMODS.calculate_effect({ message = localize('k_nope_ex'), colour = G.C.SET.Tarot, sound = 'tarot2' }, card)
+                SMODS.calculate_context({ mxms_failed_prob = true, odds = stg.odds - (stg.prob * G.GAME.probabilities.normal) })
+            end
+        end
+    end,
 }
