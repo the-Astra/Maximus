7479532704d3aa4813eb99280687d25fa47f865c
DJ Stalder <djs8743@rit.edu>
2025-06-26 00:46:51 -0400

    fixed case sensitive Black Seal issues

--- items/consumables/immortality.lua
+++ items/consumables/immortality.lua
@@ -7,11 +7,11 @@ SMODS.Consumable {
         y = 3
     },
     config = {
-        extra = 'mxms_Black'
+        extra = 'mxms_black'
     },
     cost = 4,
     loc_vars = function(self, info_queue, card)
-        info_queue[#info_queue + 1] = G.P_SEALS['mxms_Black']
+        info_queue[#info_queue + 1] = G.P_SEALS['mxms_black']
     end,
     credit = {
         art = "Maxiss02",
