fc7cf6abdc27c7d387d0b6b8da67dc160973000f
DJ Stalder <djs8743@rit.edu>
2025-06-28 21:17:54 -0400

    Improve Star Tag functionality

--- items/tags/star.lua
+++ items/tags/star.lua
@@ -20,21 +20,19 @@ if Maximus_config.horoscopes then
         end,
         apply = function(self, tag, context)
             if context.type == "new_blind_choice" then
+                local lock = tag.ID
+                G.CONTROLLER.locks[lock] = true
                 tag:yep("+", Maximus.C.SET.Horoscope, function()
-                    local key = "p_mxms_horoscope_mega_1"
-                    local card = Card(
-                        G.play.T.x + G.play.T.w / 2 - G.CARD_W * 1.27 / 2,
-                        G.play.T.y + G.play.T.h / 2 - G.CARD_H * 1.27 / 2,
-                        G.CARD_W * 1.27,
-                        G.CARD_H * 1.27,
-                        G.P_CARDS.empty,
-                        G.P_CENTERS[key],
-                        { bypass_discovery_center = true, bypass_discovery_ui = true }
-                    )
-                    card.cost = 0
-                    card.from_tag = true
-                    G.FUNCS.use_card({ config = { ref_table = card } })
-                    card:start_materialize()
+                    local booster = SMODS.create_card { key = "p_mxms_horoscope_mega_1", area = G.play, discover = true }
+                    booster.T.x = G.play.T.x + G.play.T.w / 2 - G.CARD_W * 1.27 / 2
+                    booster.T.y = G.play.T.y + G.play.T.h / 2 - G.CARD_H * 1.27 / 2
+                    booster.T.w = G.CARD_W * 1.27
+                    booster.T.h = G.CARD_H * 1.27
+                    booster.cost = 0
+                    booster.from_tag = true
+                    G.FUNCS.use_card({ config = { ref_table = booster } })
+                    booster:start_materialize()
+                    G.CONTROLLER.locks[lock] = nil
                     return true
                 end)
                 tag.triggered = true
