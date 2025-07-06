2e8178253f499bf8223d65aa61fed468658d0720
the-Astra <djs8743@rit.edu>
2025-07-06 04:39:38 -0400

    Simplify Pessimistic

--- items/jokers/pessimistic.lua
+++ items/jokers/pessimistic.lua
@@ -33,18 +33,12 @@ SMODS.Joker {
             }
         end
 
-        if context.individual and context.cardarea == G.play and not context.end_of_round and context.other_card.ability.effect == 'Lucky Card' and
-            not context.other_card.lucky_trigger and not context.blueprint then
-            stg.mult = stg.mult + card.ability.extra.lucky_gain
-            return {
-                message = localize('k_upgrade_ex'),
-                colour = G.C.ATTENTION,
-                func = function() SMODS.calculate_context({ mxms_scaling_card = true }) end
-            }
-        end
-
-        if context.mxms_probability_check and (not context.success and not mxms_is_invert_prob_check(context.card) or context.success and mxms_is_invert_prob_check(context.card)) and context.card.ability.effect ~= 'Lucky Card' and not context.blueprint then
-            stg.mult = stg.mult + (context.odds - context.prob)
+        if context.mxms_probability_check and (not context.success and not mxms_is_invert_prob_check(context.card) or context.success and mxms_is_invert_prob_check(context.card)) and not context.blueprint then
+            if context.card.ability.effect ~= 'Lucky Card' then
+                stg.mult = stg.mult + (context.odds - context.prob)
+            else
+                stg.mult = stg.mult + card.ability.extra.lucky_gain
+            end
             return {
                 message = localize('k_upgrade_ex'),
                 colour = G.C.ATTENTION,
