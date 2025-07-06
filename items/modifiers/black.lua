027625d5de6e0c055a2b3d34905d780fcedaccfa
the-Astra <djs8743@rit.edu>
2025-07-06 03:13:36 -0400

    Black seal eternal check overhaul

--- items/modifiers/black.lua
+++ items/modifiers/black.lua
@@ -34,6 +34,12 @@ SMODS.Seal {
                 x_mult = stg.Xmult
             }
         end
+
+        if context.check_eternal then
+            return {
+                no_destroy = {override_compat = true}
+            }
+        end
     end,
     in_pool = function(self, args)
         return false
@@ -65,7 +71,7 @@ Card.set_seal = function(self, _seal, silent, immediate)
     else
         css(self, _seal, silent, immediate)
         if self.seal == 'mxms_Black' and self.playing_card then
-            check_for_unlock({type = 'black_seal'})
+            check_for_unlock({ type = 'black_seal' })
         end
     end
 end
@@ -85,47 +91,6 @@ copy_card = function(other, new_card, card_scale, playing_card, strip_edition)
         SMODS.calculate_effect({ message = localize('k_not_allowed_ex'), colour = G.C.RED }, new_card)
     else
         return cc(other, new_card, card_scale, playing_card, strip_edition)
-    end
-end
-
-local csd = Card.start_dissolve
-Card.start_dissolve = function(self, dissolve_colours, silent, dissolve_time_fac, no_juice)
-    if self.seal == 'mxms_Black' and (G.STATE ~= G.STATES.SMODS_BOOSTER_OPENED and G.STATE ~= G.STATES.SHOP and not G.SETTINGS.paused or G.TAROT_INTERRUPT) then
-        SMODS.calculate_effect({ message = localize('k_mxms_destroy_block_ex'), colour = G.C.RED }, self)
-        if self.ability.set == 'Default' or self.ability.set == 'Enhanced' then
-            if G.STATE == G.STATES.SMODS_BOOSTER_OPENED then
-                draw_card(self.area, G.deck, nil, 'down', false, self)
-            else
-                draw_card(self.area, G.discard, nil, 'down', false, self)
-            end
-        end
-    else
-        csd(self, dissolve_colours, silent, dissolve_time_fac, no_juice)
-    end
-end
-
-local cr = Card.remove
-Card.remove = function(self)
-    if self.seal == 'mxms_Black' and (G.STATE ~= G.STATES.SMODS_BOOSTER_OPENED and G.STATE ~= G.STATES.SHOP and not G.SETTINGS.paused or G.TAROT_INTERRUPT) then
-        SMODS.calculate_effect({ message = localize('k_not_allowed_ex'), colour = G.C.RED }, self)
-    else
-        cr(self)
-    end
-end
-
-local cs = Card.shatter
-Card.shatter = function(self)
-    if self.seal == 'mxms_Black' and (G.STATE ~= G.STATES.SMODS_BOOSTER_OPENED and G.STATE ~= G.STATES.SHOP and not G.SETTINGS.paused or G.TAROT_INTERRUPT) then
-        SMODS.calculate_effect({ message = localize('k_mxms_destroy_block_ex'), colour = G.C.RED }, self)
-        if self.ability.set == 'Default' or self.ability.set == 'Enhanced' then
-            if G.STATE == G.STATES.SMODS_BOOSTER_OPENED then
-                draw_card(self.area, G.deck, nil, 'down', false, self)
-            else
-                draw_card(self.area, G.discard, nil, 'down', false, self)
-            end
-        end
-    else
-        cs(self)
     end
 end
 
