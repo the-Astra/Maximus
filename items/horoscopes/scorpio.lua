53b9015113d0bf7355cdafd6a94ca629604c9ff2
the-Astra <djs8743@rit.edu>
2025-08-05 13:41:23 -0400

    Scorpio cannot appear as the first Horoscope in Zodiac Killer

--- items/horoscopes/scorpio.lua
+++ items/horoscopes/scorpio.lua
@@ -79,7 +79,7 @@ SMODS.Consumable {
     end,
     in_pool = function(self, args)
         if G.GAME.modifiers.mxms_zodiac_killer then
-            return G.GAME.zodiac_killer_pools["Scorpio"]
+            return G.GAME.zodiac_killer_pools["Scorpio"] and G.GAME.round_resets.ante > 1
         end
         return true
     end,
