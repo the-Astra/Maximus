55de28c55e76b0cc39879d84a9b1001ca5788213
the-Astra <djs8743@rit.edu>
2025-08-10 13:05:26 -0400

    Update H&C draw method to use should_hide_front

--- items/jokers/hammer_and_chisel.lua
+++ items/jokers/hammer_and_chisel.lua
@@ -51,69 +51,8 @@ function Card:set_ability(center, initial, delay_sprites)
     end
 end
 
--- Main front drawstep
-SMODS.DrawStep {
-    key = 'HnC_Front',
-    order = 1,
-    func = function(self, layer)
-        --Draw the main part of the card
-        if (self.edition and self.edition.negative and not self.delay_edition) or (self.ability.name == 'Antimatter' and (self.config.center.discovered or self.bypass_discovery_center)) then
-            if self.children.front and self.ability.effect == 'Stone Card' and next(SMODS.find_card('j_mxms_hammer_and_chisel')) then
-                self.children.front:draw_shader('negative', nil, self.ARGS.send_to_shader)
-            end
-        elseif not self:should_draw_base_shader() then
-            -- Don't render base dissolve shader.
-        elseif not self.greyed then
-            if self.children.front and self.ability.effect == 'Stone Card' and next(SMODS.find_card('j_mxms_hammer_and_chisel')) then
-                self.children.front:draw_shader('dissolve')
-            end
-        end
-    end,
-    conditions = { vortex = false, facing = 'front' },
-}
-
--- Edition drawstep
-SMODS.DrawStep {
-    key = 'HnC_Edition',
-    order = 21,
-    func = function(self, layer)
-        if self.edition and not self.delay_edition then
-            for k, v in pairs(G.P_CENTER_POOLS.Edition) do
-                if self.edition[v.key:sub(3)] and v.shader then
-                    if type(v.draw) == 'function' then
-                        v:draw(self, layer)
-                    else
-                        if self.children.front and self.ability.effect == 'Stone Card' and next(SMODS.find_card('j_mxms_hammer_and_chisel')) then
-                            self.children.front:draw_shader(v.shader, nil, self.ARGS.send_to_shader)
-                        end
-                    end
-                end
-            end
-        end
-    end,
-    conditions = { vortex = false, facing = 'front' },
-}
-
--- Debuff drawstep
-SMODS.DrawStep {
-    key = 'HnC_Debuff',
-    order = 71,
-    func = function(self, layer)
-        if self.debuff and self.children.front and self.ability.effect == 'Stone Card' and next(SMODS.find_card('j_mxms_hammer_and_chisel')) then
-            self.children.front:draw_shader('debuff', nil, self.ARGS.send_to_shader)
-        end
-    end,
-    conditions = { vortex = false, facing = 'front' },
-}
-
--- Greyed drawstep
-SMODS.DrawStep {
-    key = 'HnC_Greyed',
-    order = 81,
-    func = function(self, layer)
-        if self.greyed and self.children.front and self.ability.effect == 'Stone Card' and next(SMODS.find_card('j_mxms_hammer_and_chisel')) then
-            self.children.front:draw_shader('played', nil, self.ARGS.send_to_shader)
-        end
-    end,
-    conditions = { vortex = false, facing = 'front' },
-}
+local shf = Card.should_hide_front
+function Card:should_hide_front()
+    if self.ability.effect == 'Stone Card' and next(SMODS.find_card('j_mxms_hammer_and_chisel')) then return false end
+    shf(self)
+end
