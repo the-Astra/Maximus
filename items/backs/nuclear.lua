de00d5b507f7b9abd7ad64565be58c186025ac2a
the-Astra <djs8743@rit.edu>
2025-08-14 21:55:16 -0400

    Nuke deck profile high score prevention now a hook

--- items/backs/nuclear.lua
+++ items/backs/nuclear.lua
@@ -21,3 +21,15 @@ SMODS.Back {
         G.GAME.starting_params.joker_slots = G.GAME.starting_params.joker_slots - 4
     end
 }
+
+local cashs = check_and_set_high_score
+function check_and_set_high_score(score, amt)
+    if G.GAME.modifiers.mxms_nuclear_size then
+        if not amt or type(amt) ~= 'number' then return end
+        if G.GAME.round_scores[score] and math.floor(amt) > (G.GAME.round_scores[score].amt or 0) then
+            G.GAME.round_scores[score].amt = math.floor(amt)
+        end
+        return
+    end
+    cashs(score, amt)
+end
