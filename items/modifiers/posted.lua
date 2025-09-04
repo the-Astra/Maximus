0b5fa52f79f58a974845cc0c86a7dd71af95086a
the-Astra <djs8743@rit.edu>
2025-09-03 20:36:27 -0400

    Posted default compat

--- items/modifiers/posted.lua
+++ items/modifiers/posted.lua
@@ -3,6 +3,7 @@ SMODS.Sticker {
     badge_colour = HEX 'fda200',
     pos = { x = 10, y = 10 },
     rate = 0,
+    default_compat = false,
     apply = function(self, card, val)
         card.ability[self.key] = val
     end
