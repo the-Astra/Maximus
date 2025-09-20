31be4c3cc18f73d0020ea903a1e1a34510e6df60
the-Astra <djs8743@rit.edu>
2025-09-20 00:39:12 -0400

    Leftovers uses joker_type_destroyed now

--- items/jokers/leftovers.lua
+++ items/jokers/leftovers.lua
@@ -16,49 +16,32 @@ SMODS.Joker {
     pools = {
         Food = true
     },
-    rarity = 1
-}
-
---Leftovers food detection
-local remove_ref = Card.remove
-function Card.remove(self)
-    if self.added_to_deck and self.ability.set == 'Joker' and not G.CONTROLLER.locks.selling_card and self.config.center_key ~= 'j_mxms_leftovers' then
-        local first_leftovers = SMODS.find_card('j_mxms_leftovers')[1]
-        if first_leftovers and Maximus.is_food(self) then
-            local respawn_key = self.config.center_key
-
-            play_sound('timpani')
-
-            SMODS.add_card({
-                set = 'Joker',
-                key = respawn_key,
-                key_append = 'lefto'
-            })
-
-            first_leftovers.T.r = -0.2
-            first_leftovers:juice_up(0.3, 0.4)
-            first_leftovers.states.drag.is = true
-            first_leftovers.children.center.pinch.x = true
-
-            SMODS.calculate_effect(
-                { message = localize('k_mxms_saved_later_ex'), colour = G.C.FILTER, sound = 'tarot1' },
-                first_leftovers)
-
+    rarity = 1,
+    calculate = function(self, card, context)
+        local stg = card.ability.extra
+    
+        if context.joker_type_destroyed and context.card.config.center.pools.Food and not context.card.config.center_key == 'j_mxms_leftovers' then
+            local respawn_key = context.card.config.center_key
             G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.3,
-                blockable = false,
                 func = function()
-                    G.jokers:remove_card(first_leftovers)
-                    first_leftovers:remove()
-                    first_leftovers = nil
+                    SMODS.add_card {
+                        key = respawn_key,
+                        key_append = 'lefto'
+                    }
                     return true;
                 end
             }))
+
+            SMODS.destroy_cards(card, nil, nil, true)
+
+            return {
+                message = 'k_mxms_saved_later_ex',
+                colour = G.C.FILTER,
+                sound = 'tarto1'
+            }
         end
-    end
-    return remove_ref(self)
-end
+    end,
+}
 
 SMODS.JimboQuip {
     key = 'lq_leftovers',
