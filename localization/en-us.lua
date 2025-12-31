fa80ab303a32ba036ba4332a5b6350c695490e32
the-Astra <djs8743@rit.edu>
2025-12-30 22:04:12 -0500

    Black Seal better clarity

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1809,7 +1809,7 @@ return {
                 text = {
                     "{X:mult,C:white}X#1#{} Mult,",
                     "Card cannot be {C:attention}modified",
-                    "and is {C:dark_edition,E:1}eternal"
+                    "and or {C:red}destroyed"
                 }
             },
             mxms_credits = {
