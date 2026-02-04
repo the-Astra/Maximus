00924ac5f0c6b9a3164f22f7ac50257aa1f63cbd
the-Astra <djs8743@rit.edu>
2026-02-03 19:40:30 -0500

    Virus uses a hook instead of a patch

--- items/jokers/virus.lua
+++ items/jokers/virus.lua
@@ -14,3 +14,30 @@ SMODS.Joker {
     blueprint_compat = false,
     cost = 6
 }
+
+local gf = get_flush
+function get_flush(hand)
+    local ret = {}
+    local suits = SMODS.Suit.obj_buffer
+    if next(SMODS.find_card('j_mxms_virus')) then
+        if #hand < 2 then
+            -- Do nothing
+        else
+            for j = 1, #suits do
+                local t = {}
+                local suit = suits[j]
+                local flush_count = 0
+                for i = 1, #hand do
+                    if hand[i]:is_suit(suit, nil, true) then
+                        flush_count = flush_count + 1; t[#t + 1] = hand[i]
+                    end
+                end
+                if flush_count == #hand and flush_count >= 2 then
+                    table.insert(ret, t)
+                    return ret
+                end
+            end
+        end
+    end
+    return gf(hand)
+end
