2183b2d9e9b93bc69223500dfe72458e8c7f058b
the-Astra <djs8743@rit.edu>
2026-03-01 15:53:29 -0500

    Forgot this

--- items/backs/sleeves/destiny.lua
+++ items/backs/sleeves/destiny.lua
@@ -48,7 +48,8 @@ if Maximus_config.horoscopes then
                         return true
                     end
                 }))
-                G.GAME.modifiers.mxms_horoscope_ante_end = 'p_mxms_horoscope_mega_1'
+                G.GAME.modifiers.mxms_booster_ante_end = G.GAME.modifiers.mxms_booster_ante_end or {}
+                table.insert(G.GAME.modifiers.mxms_booster_ante_end, 'p_mxms_horoscope_mega_1')
             end
         end
     }
