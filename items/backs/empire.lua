7f43d49ba7b65ce66fdde6030d8c69a493781fca
the-Astra <djs8743@rit.edu>
2026-04-08 15:47:03 -0400

    Empire deck now uses calculate instead of taking over

--- items/backs/empire.lua
+++ items/backs/empire.lua
@@ -10,13 +10,25 @@ SMODS.Back {
         code = { "theAstra" },
         idea = { "pinkzigzagoon" }
     },
-    apply = function(self, back)
-        --Deck modifier
-        G.GAME.modifiers.mxms_empire_arcana = true
-    end,
     calculate = function(self, back, context)
         if context.mxms_pre_choice_modify and context.booster.kind == 'Arcana' then
             context.card.ability.extra = SMODS.size_of_pool(get_current_pool('Tarot'))
+        end
+
+        if context.create_booster_card and context.booster.config.center.kind == 'Arcana' then
+            local pool = SMODS.get_clean_pool('Tarot')
+
+            return {
+                booster_create_flags = {
+                    set = "Tarot",
+                    area = G.pack_cards,
+                    skip_materialize = true,
+                    soulable = true,
+                    key = pool[context.index],
+                    key_append =
+                    'ar1'
+                }
+            }
         end
     end,
 }
